46d1f41c4cd851a50f22c71947c6336a767f0df9
Dmitrii Shenderovich <dimartart3@gmail.com>
2025-07-16 20:57:13 +0200

    sc

--- src/pages/CancelReservation.jsx
+++ src/pages/CancelReservation.jsx
@@ -3,7 +3,7 @@ import { supabase } from "../supabaseClient";
 
 const CancelReservation = () => {
     return (
-        <div>
+        <div className="bg-gradient-to-br from-gray-900 via-black to-purple-900 border-b border-[#D41C8A] relative flex flex-col lg:flex-row mt-10 px-4 sm:px-6 lg:px-[10%] py-8 md:py-12 lg:py-[4rem] lg:pb-[7rem]">
             <button onClick={async () => {
                 await supabase
                 .from('reservations')
--- src/pages/EditReservation.jsx
+++ src/pages/EditReservation.jsx
@@ -5,7 +5,7 @@ const EditReservation = () => {
     const { id } = useParams();
 
     return (    
-        <div>
+        <div className="bg-gradient-to-br from-gray-900 via-black to-purple-900 border-b border-[#D41C8A] relative flex flex-col lg:flex-row mt-10 px-4 sm:px-6 lg:px-[10%] py-8 md:py-12 lg:py-[4rem] lg:pb-[7rem]">
             <button>Edit reservation</button>
         </div>
     )
