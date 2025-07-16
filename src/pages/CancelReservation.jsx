48f89a6608f112c494212b4192e2099f5f3bcda2
Dmitrii Shenderovich <dimartart3@gmail.com>
2025-07-16 21:04:12 +0200

    sc

--- src/pages/CancelReservation.jsx
+++ src/pages/CancelReservation.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom';
 import { supabase } from "../supabaseClient";
 
 const CancelReservation = () => {
+    const { modification_token } = useParams();
+
+
     return (
         <div className="bg-gradient-to-br from-gray-900 via-black to-purple-900 border-b border-[#D41C8A] relative flex flex-col lg:flex-row mt-10 px-4 sm:px-6 lg:px-[10%] py-8 md:py-12 lg:py-[4rem] lg:pb-[7rem]">
             <button className="text-white text-2xl font-bold" onClick={async () => {
