be2850900b450bf558387f3fe3fd009923c40ecc
Dmitrii Shenderovich <dimartart3@gmail.com>
2025-07-16 20:59:03 +0200

    sc

--- src/pages/CancelReservation.jsx
+++ src/pages/CancelReservation.jsx
@@ -4,7 +4,7 @@ import { supabase } from "../supabaseClient";
 const CancelReservation = () => {
     return (
         <div className="bg-gradient-to-br from-gray-900 via-black to-purple-900 border-b border-[#D41C8A] relative flex flex-col lg:flex-row mt-10 px-4 sm:px-6 lg:px-[10%] py-8 md:py-12 lg:py-[4rem] lg:pb-[7rem]">
-            <button onClick={async () => {
+            <button className="text-white text-2xl font-bold" onClick={async () => {
                 await supabase
                 .from('reservations')
                 .delete()
