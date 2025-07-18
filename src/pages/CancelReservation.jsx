a83f2793c8312f1d258ea47205d3df275d2f6eb9
Dmitrii Shenderovich <dimartart3@gmail.com>
2025-07-18 17:38:40 +0200

    sc

--- src/pages/CancelReservation.jsx
+++ src/pages/CancelReservation.jsx
@@ -7,12 +7,18 @@ const CancelReservation = () => {
 
     return (
         <div className="bg-gradient-to-br from-gray-900 via-black to-purple-900 border-b border-[#D41C8A] relative flex flex-col lg:flex-row mt-10 px-4 sm:px-6 lg:px-[10%] py-8 md:py-12 lg:py-[4rem] lg:pb-[7rem]">
-            <button className="text-white text-2xl font-bold" onClick={async () => {
-                await supabase
-                .from('reservations')
-                .delete()
-                .eq('modification_token', modification_token);
-                alert('Reservation cancelled.');
+            <button className="text-white text-2xl font-bold" onClick={
+                async () => {
+                    const { data, error } = await supabase
+                    .from('reservation')
+                    .delete()
+                    .eq('modification_token', modification_token);
+
+                    if (error) {
+                        throw error;
+                    }
+
+                    alert('Reservation cancelled.');
                 }}>
                 Cancel reservation
             </button>
